93cda55e4ee1412d10d2eb1876f77826a7195acf
UDay <uday.dev.bd@gmail.com>
2023-02-25 02:58:40 +0600

    add prisma

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,33 @@
-import express, { Application,Request,Response,NextFunction } from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
+
+import express, {
+  Application,
+  Request,
+  Response,
+  NextFunction,
+} from "express";
+// import { pool } from "./db";
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app: Application = express();
 
-app.get("/",(req:Request,res:Response,next:NextFunction)=> res.json("bms auth service"))
+app.get("/", async (req: Request, res: Response, next: NextFunction) => {
+  const data = await prisma.user.findMany();
+  return res.json(data);
+});
+app.get("/add", async (req: Request, res: Response, next: NextFunction) => {
+  const data = await prisma.user.create({
+    data:{
+        name:"jui",
+        email:"jui@gmail.com"
+    }
+  });
+  return res.json(data);
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`auth server is running on ${PORT}`));
