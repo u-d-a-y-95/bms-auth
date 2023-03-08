c7c4c3b532f66941ad466894633c6b5fa374a4a6
UDay <uday.dev.bd@gmail.com>
2023-03-08 17:21:22 +0600

    add bcrypt

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -7,8 +7,8 @@ export const login = async (req: Request, res: Response) => {
   const { mobile, password } = req.body;
   const user = await getOne(mobile);
   if (!user) return res.status(400).json("unauthentic");
-  //   const isMatch = await compareHash(password, user.password);
-  //   if (!isMatch) return res.status(400).json("unauthentic");
+  const isMatch = await compareHash(password, user.password);
+  if (!isMatch) return res.status(400).json("unauthentic");
   const { password: _password, ...rest } = user;
   const token = await generateJwtToken(rest);
   return res.json({
